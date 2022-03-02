6f279b00292d29b12af9bfa12c08012ac078f9c7
AndersonDiasSampaio <ads@ic.ufal.br>
2022-03-02 18:57:16 -0300

    Agora o programa usa Orientação a objetos e o código antigo está comentado.

--- src/application/ProgramTriangulo.java
+++ src/application/ProgramTriangulo.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.*;
 
+import Entity.Triangulo;
 public class ProgramTriangulo {
 
 	public static void main(String[] args) {
@@ -11,28 +12,41 @@ public class ProgramTriangulo {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite as dimensões do primeiro triangulo");
-		xA = sc.nextDouble();
+	/*	xA = sc.nextDouble();
 		xB = sc.nextDouble();
 		xC = sc.nextDouble();
-		System.out.println("Digite as dimensões do segundo triangulo");
 		yA = sc.nextDouble();
 		yB = sc.nextDouble();
 		yC = sc.nextDouble();
 		p = (xA + xB + xC) / 2;
 		area = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		p1 = (yA + yB + yC) / 2;
-		area = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
-		if (p > p1)
+		area = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));*/
+		Triangulo x, y;
+		x= new Triangulo();
+		y= new Triangulo();
+		x.setA(sc.nextDouble());
+		x.setB(sc.nextDouble());
+		x.setC(sc.nextDouble());
+		System.out.println("Digite as dimensões do segundo triangulo");
+
+		y.setA(sc.nextDouble());
+		y.setB(sc.nextDouble());
+		y.setC(sc.nextDouble());
+		System.out.println(x.getA());
+
+		if (x.area() > y.area())
 
 		{
-			System.out.println("O primeiro triangulo é maior");
 
-		} else if (p == p1) {
-			System.out.println("A área dos 2 triangulos são iguais");
+		} else if (x.area() == y.area()) {
+			System.out.println("A área dos 2 triangulos são iguais" + x.area()+"segunda: "+y.area());
 		} else {
 			System.out.println("O segundo triangulo é maior");
 
 		}
+		
+
 	}
 
 }
