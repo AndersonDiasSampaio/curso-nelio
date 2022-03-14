be9b722e4b44e4d8c009400ba6e03e385e4af7ca
AndersonDiasSampaio <ads@ic.ufal.br>
2022-03-14 17:04:53 -0300

    classe produto criada

---
+++ src/Entity/Produto.java
@@ -0,0 +1,33 @@
+package Entity;
+
+public class Produto {
+String Name;
+double Price;
+int Quantity;
+public Produto(String name, double price) {
+	Name = name;
+	Price = price;
+}
+public Produto() {
+	
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public double getPrice() {
+	return Price;
+}
+public void setPrice(double price) {
+	Price = price;
+}
+public int getQuantity() {
+	return Quantity;
+}
+public void setQuantity(int quantity) {
+	Quantity = quantity;
+}
+
+}
